62a3792beca29d140e308ffefa974bdfe96144bd
Heitorado <heitor.scz@gmail.com>
2017-10-03 22:17:14 -0300

    add todolist todos, and some visual changes in css/html on both sender and receiver pages

--- public/javascripts/scktReceiver.js
+++ public/javascripts/scktReceiver.js
@@ -3,7 +3,7 @@ var socket = io();
 
 //trata o recebimento da propagação do evento pelo servidor
 socket.on('chat message', function(msg){
-  $('#msgCards').append($('<div class="exercise-card">').append($('<div class="card-container">')).append($('<h4><b>Mensagem:</b></h4>').append($('<p></p>').text(msg))));
+  $('#msgCards').append($('<div class="exercise-card">').append($('<div class="card-container">')).append($('<h4><b>Prece recebida:</b></h4>')).append($('<div id="msgRecvd"></div>').append($('<p></p>').text(msg))));
 
 
   console.log(msg);
