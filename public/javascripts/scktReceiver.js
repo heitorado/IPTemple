37ce92f5f1b710975b4268d7408ed64f6de46ff3
Victor Aurelio <victor@viitra.com>
2017-10-04 21:08:40 -0300

    make changes to receiver style, topbar and receiver.js

--- public/javascripts/scktReceiver.js
+++ public/javascripts/scktReceiver.js
@@ -4,10 +4,10 @@ var socket = io();
 //trata o recebimento da propagação do evento pelo servidor
 socket.on('chat message', function(msg){
 
-	var newCard = $('<div class="exercise-card">').append($('<div class="card-container">')).append($('<h4><b>Prece recebida:</b></h4>')).append($('<div id="msgRecvd"></div>').append($('<p></p>').text(msg)));
+	//var newCard = $('<div class="exercise-card">').append($('<div class="card-container">')).append($('<h4><b>Prece recebida:</b></h4>')).append($('<div id="msgRecvd"></div>').append($('<p></p>').text(msg)));
   
- 	$(newCard).hide().appendTo('#msgCards').fadeTo(100,1);
-
+ 	//$(newCard).hide().appendTo('#msgCards').fadeTo(100,1);
+	$('#msgCards').append($('<hr></hr><p></p>').text(msg)).fadeTo(100,1);
 
   console.log(msg);
   window.scrollBy(0, 1000000000); //desce a janela
